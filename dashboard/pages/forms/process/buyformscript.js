da2db03e266e7328a9873ba951fb94bcf44dc7c3
Victor Okafor <okaffskido@gmail.com>
2024-10-02 22:05:56 +0100

    disabled paystack

--- dashboard/pages/forms/process/buyformscript.js
+++ dashboard/pages/forms/process/buyformscript.js
@@ -1,24 +1,22 @@
 
-    document.getElementById('paymentForm').addEventListener("submit", function(e) {
-        e.preventDefault(); // Prevent form submission
+    // document.getElementById('paymentForm').addEventListener("submit", function(e) {
+    //     e.preventDefault(); 
+    //     let handler = PaystackPop.setup({
+    //         key: 'pk_test_d19b75766790acc8c1c19aa8d6f28cbbbab0e1bf', 
+    //         email: document.getElementById("email-address").value,
+    //         amount: document.getElementById("amount").value * 100,
+    //         ref: 'RUBY' + Math.floor((Math.random() * 1000000000) + 1),
+    //         onClose: function () {
+    //             window.location = "http://localhost:60/localserver/gm-talent/dashboard/";
+    //             alert('Transaction Cancelled.');
+    //         },
+    //         callback: function (response) {
+    //             window.location = "verifyPayment.php?ref="+response.reference;
 
-        // Your Paystack initialization code here
-        let handler = PaystackPop.setup({
-            key: 'pk_test_d19b75766790acc8c1c19aa8d6f28cbbbab0e1bf', 
-            email: document.getElementById("email-address").value,
-            amount: document.getElementById("amount").value * 100,
-            ref: 'RUBY' + Math.floor((Math.random() * 1000000000) + 1),
-            onClose: function () {
-                window.location = "http://localhost:60/localserver/gm-talent/dashboard/";
-                alert('Transaction Cancelled.');
-            },
-            callback: function (response) {
-                window.location = "verifyPayment.php?ref="+response.reference;
+    //         }
+    //     });
 
-            }
-        });
-
-        handler.openIframe();
-    });
+    //     handler.openIframe();
+    // });
 
 
